8937b99f97b65c3d86bd63a8a5a4a166c52f82d6
gabss 3 <99218597+zgbsss@users.noreply.github.com>
2023-03-08 16:50:13 -0300

    Add files via upload

--- contadorApp/src/components/Counter.js
+++ contadorApp/src/components/Counter.js
@@ -1,13 +1,14 @@
 import { Component } from "react";
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class Counter extends Component {
     state = {
         count: 0,
+        step: 2,
     };
     incrementar(){
         this.setState({
-            count: this.state.count + 1,
+            count: this.state.count + this.state.step,
         });
     }
     decrementar(){
@@ -17,9 +18,23 @@ export default class Counter extends Component {
             });
         }
     }
+    alteraStep(text) {
+        this.setState ({step: parseInt(text)})
+    }
     render() {
         return (
             <View style={styles.contador}>
+                <TextInput value={this.state.step.toString()}
+                onChange={(e) => {
+                    this.alteraStep(e.nativeEvent.text);
+                }}
+                 />
+                <TextInput value={this.state.step.toString()}
+                onChange={(valor) => {
+                    this.alteraStep(valor);
+                }}
+                 />
+                 
                 <Text>Contador: {this.state.count}</Text>
                 <Button onPress={() => this.incrementar()} title="+" />
                 <Button onPress={() => this.decrementar()} title="-" />
